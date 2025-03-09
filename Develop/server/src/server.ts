a168150d9dd99ed061114c789ba241f908f40b22
M-Dearlove <dearlove.matthew@gmail.com>
2025-03-09 19:45:52 -0400

    server.ts update

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -3,9 +3,13 @@ import path from 'node:path';
 import db from './config/connection.js';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import { fileURLToPath } from 'node:url';
 
 // Import the two parts of a GraphQL schema
 import { typeDefs, resolvers } from './schemas/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = new ApolloServer({
   typeDefs,
@@ -34,7 +38,7 @@ const startApolloServer = async () => {
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
   }
-  
+
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
