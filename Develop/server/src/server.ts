505a6d648e51b779b08bc0a1efd9f683727f12a1
M-Dearlove <dearlove.matthew@gmail.com>
2025-03-11 19:30:15 -0400

    updated main.tsx

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -42,7 +42,7 @@ const startApolloServer = async () => {
   }
 
   app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 
   app.listen(PORT, () => {
